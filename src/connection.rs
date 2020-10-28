c6e9f03617048df12bdfbc56600390cfdc644d4d
lyons <hi@klyons.ca>
2020-10-28 15:03:44 -0600

    Functionality stubbed in for response handler to look through list of possible index files when serving a directory

--- src/connection.rs
+++ src/connection.rs
@@ -30,10 +30,8 @@ use crate::status::Status;
 type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
 
 pub async fn handle_connection(acceptor: &TlsAcceptor, tcp_stream: &mut TcpStream) -> Result<()> {
-  let peer_addr = tcp_stream.peer_addr()?;
-  let local_addr = tcp_stream.local_addr()?;
-  println!("Connection from {}", peer_addr);
-  println!("Connection to {}", local_addr);
+  //let peer_addr = tcp_stream.peer_addr()?;
+  //println!("Connection from {}", peer_addr);
 
   let handshake = acceptor.accept(tcp_stream);
   let mut tls_stream = handshake.await?;
@@ -49,43 +47,66 @@ pub async fn handle_connection(acceptor: &TlsAcceptor, tcp_stream: &mut TcpStrea
   Ok(())
 }
 
-async fn handle_request<W: Write + Unpin>(request_url: &Url, tls_stream: &mut W) -> Result<()> {
+async fn handle_request<W: Write + Unpin>(request_url: &Url, stream: &mut W) -> Result<()> {
   let mut request_path = PathBuf::from("/var/gemini/");
   request_path.extend(request_url.path_segments().unwrap());
   println!("Request URL: {}", request_url);
   println!("Request path: {:?}", request_path);
 
   if request_path.is_dir().await {
-    if request_url.as_str().ends_with("/") {
-      request_path.push("index.gmi");
-    }
-    else {
+    if !request_url.as_str().ends_with("/") 
+    {
       let mut redirect_url = String::from(request_url.as_str());
       redirect_url.push_str("/");
-      write_header(tls_stream, Status::RedirectPermanent, &redirect_url).await?;
-      return Ok(())
+      write_header(stream, Status::RedirectPermanent, &redirect_url).await?;
+    }
+    else {
+      let mut index_found = false;
+      for filename in get_index_names() {
+        request_path.push(filename);
+        if request_path.exists().await {
+          index_found = true;
+          break;
+        }
+        else {
+          request_path.pop();
+        }
+      }
+      
+      if index_found {
+        serve_file(request_path, stream).await?;
+      }
+      else {
+        write_header(stream, Status::TemporaryFailure, "Forbidden").await?;
+      }
     }
   }
   
-  if request_path.exists().await {
-    let mimetype = get_mimetype(request_path.extension());
-    let mut file = File::open(request_path).await?;
-    write_header(tls_stream, Status::Success, mimetype).await?;
-    async_std::io::copy(&mut file, tls_stream).await?;
+  else if request_path.exists().await {
+    serve_file(request_path, stream).await?;
   }
+
   else {
-    write_header(tls_stream, Status::NotFound, "File not found").await?;
+    write_header(stream, Status::NotFound, "File not found").await?;
   }
 
   Ok(())
 }
 
-async fn parse_request<R: Read + Unpin>(tls_stream: &mut R) -> Result<Url> {
+async fn serve_file<W: Write + Unpin>(path: PathBuf, stream: &mut W) -> Result<()> {
+  let mimetype = get_mimetype(path.extension());
+  let mut file = File::open(path).await?;
+  write_header(stream, Status::Success, mimetype).await?;
+  async_std::io::copy(&mut file, stream).await?;
+  Ok(())
+}
+
+async fn parse_request<R: Read + Unpin>(stream: &mut R) -> Result<Url> {
   let mut buffer = [0; 1026];
   let mut length = 0;
 
   loop {
-    let read_count = tls_stream.read(&mut buffer[length..]).await?;
+    let read_count = stream.read(&mut buffer[length..]).await?;
     length += read_count;
     if buffer[..length].ends_with(b"\r\n") {
       break;
@@ -129,4 +150,8 @@ fn get_mimetype(extension: Option<&OsStr>) -> &'static str {
     },
     None => "text/plain",
   }
+}
+
+fn get_index_names() -> &'static [&'static str] {
+  &["index.gemini", "index.gmi"]
 }
